43e57f069095dcda6a8997bf8012036e2aef3abe
BCAndreaC <bc.andreac@gmail.com>
2023-06-21 07:56:48 -0500

    Test unitarios, CLI stats y stats con validate

--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,6 @@
 
 const {mdLinks} = require('./index.js');
 const pathUser = process.argv[2];
-const option1 = process.argv[3];
-const option2 = process.argv[4];
 
 const { uniqueLinks, brokenLinks} = require('./util.js');
 const colors = {
@@ -14,9 +12,10 @@ const colors = {
     fgYellow: '\x1b[33m',
     bgBlue: '\x1b[44m',
   };
+const options = process.argv;
 
 
-if (option1.includes('--validate')) {
+if (options.includes('--validate') && !options.includes('--stats')) {
     mdLinks(pathUser, { validate: true })
       .then((mdFiles) => {
         mdFiles.forEach((link) => {
@@ -29,24 +28,29 @@ if (option1.includes('--validate')) {
           console.log(colors.fgGreen,'------------------------------', colors.reset);
         });
       });
-  } else if (option1.includes('--stats')) {
-    mdLinks(pathUser, { validate: false })
-      .then((links) => {
-        console.log('File:', links.file);
-        console.log('Total:', links.length);
-        console.log('Unique:', uniqueLinks(links).length);
+  } else if (!options.includes('--validate') && options.includes('--stats')) {
+    mdLinks(pathUser, { stats: true })
+      .then((mdFiles) => {
+        const uniqueLink = uniqueLinks(mdFiles);
+        console.log(colors.fgMagenta,'Total:', mdFiles.length, colors.reset);
+        console.log(colors.fgCyan,'Unique:',uniqueLink.length, colors.reset);
       });
-  } else if (option1.includes('--stats') && option2.includes('--validate')) {
-    mdLinks(pathUser, { validate: true })
-      .then(() => {
-        console.log('File:', filePath);
-        console.log('Total:', totalLinks);
-        console.log('Unique:', uniqueLinks);
-        console.log('Broken:', brokenLinks);
+  } else if (options.includes('--stats') && options.includes('--validate')) {
+    mdLinks(pathUser, { stats: true, validate: true  })
+      .then((mdFiles) => {
+        const uniqueLink = uniqueLinks(mdFiles);
+        const brokenLink = brokenLinks(mdFiles);
+        console.log(colors.fgMagenta,'Total:', mdFiles.length, colors.reset);
+        console.log(colors.fgCyan,'Unique:', uniqueLink.length, colors.reset);
+        console.log(colors.fgRed,'Broken:', brokenLink.length, colors.reset);
       });
-  } else {
+  } else if (!options.includes('--validate') && !options.includes('--stats')) {
+    mdLinks(pathUser, { validate: false  })
+      .then((mdFiles) => {
     console.log('------------------------------');
-    console.log('You can use --validate, --stats, or --validate --stats.');
+    console.log(colors.fgCyan,'Links: ',colors.reset,mdFiles ,colors.fgCyan, 'File: ',colors.fgMagenta, pathUser,colors.reset);
     console.log('------------------------------');
-  }
+       });
+};
   
+
